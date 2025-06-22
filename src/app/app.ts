c3dbdc78aea279411f47b4226250a76ec95eefb9
Suciu Tudor <suciu532@gmail.com>
2025-06-22 14:03:27 +0300

    feat: add home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
